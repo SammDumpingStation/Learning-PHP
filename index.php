85e7bc2222d6ec2d7cfa984c6ffc2100281ec794
Samm Caagbay <sammcaag@gmail.com>
2024-04-30 22:11:22 +0800

    👍 Simple Log-in Form (Version 1)

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    <label for="">Username:</label><br>
+    <input type="text" name="username"> <br>
+    <label for="">Password</label> <br>
+    <input type="password" name="password"><br>
+    <input type="submit" value="Log in" name="login">
+  </form>
+</body>
+</html>
+
+<?php
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if (empty($username)) {
+    echo "You have not entered a username. <br>";
+  }
+  else {
+    echo "Username: {$username} <br>";
+  }
+  if (empty($password)) {
+    echo "You have not entered a password <br>";
+  } 
+  else {
+    echo "Password: {$password} <br>";
+  }
+?>
