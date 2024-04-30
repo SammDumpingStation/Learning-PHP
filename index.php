0221f1b4c5f8328077dd98bfdfdca230cf2a1e95
Samm Caagbay <sammcaag@gmail.com>
2024-04-30 10:37:10 +0800

    👍 Trying out the different MATH functions in PHP (Absolute)

--- index.php
+++ index.php
@@ -6,19 +6,25 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="index.php" method="post">
-    <label for="">Quantity: </label> <br>
-    <input type="text" name="quantity"> <br>
-    <input type="submit" value="Total">
+  <form action="index.php" method="get">
+    <label>X:</label> 
+    <input type="text" name="X"><br>
+    <label>Y:</label>
+    <input type="text" name="Y"><br>
+    <label>Z:</label>
+    <input type="text" name="Z"><br>
+    <input type="submit" value="Calculate">
   </form>
 </body>
 </html>
 
-<?php 
-  $price = 5.99;
-  $quant = $_POST['quantity'];
-  $total = $price * $quant;
+<?php
+  $x = $_GET['X'];
+  $Y = $_GET['Y'];
+  $Z = $_GET['Z'];
+  $total = null;
 
-  echo"You have ordered {$quant} xa of Pizza/s <br>";
-  echo"Your total is: \${$total}";
+  $total = abs($x);
+  echo $total;
+
 ?>
