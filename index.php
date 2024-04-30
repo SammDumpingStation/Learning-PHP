cae7353b0d9a042a5578a29098c124c6128d476a
Samm Caagbay <sammcaag@gmail.com>
2024-04-30 10:34:16 +0800

    👍 Learning the syntax of PHP

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 
   $gpa = 1.5;
 
+  $total = null;
+
   // String Variables
   echo"I Like {$food}! <br>";
   echo"I am {$name} <br>";
@@ -17,10 +19,15 @@
   // Integer Variables
   echo"I am {$age} years old <br>";
   echo"There are {$users} online users <br>";
-  echo"There are {$quant} available. Would you like to purchase?<br>";
+  echo"There are {$quant}  available. Would you like to purchase?<br>";
 
   // Float Variables
-  echo"I averaged {$gpa} this school year";
+  echo"I averaged {$gpa} this school year <br>";
+
+  // Arithmetic Operation
+  echo"You have purchased {$quant} x {$food}'s <br>";
+  $total = $quant * $users;
+  echo"Your total is {$quant} x {$users} = \${$total}";
 ?>
 <!DOCTYPE html>
 <html lang="en">
