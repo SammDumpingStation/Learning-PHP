0db722b684a7b3543406b5a8fa4d62e63e1b11e4
Samm Caagbay <sammcaag@gmail.com>
2024-05-10 20:38:52 +0800

    👍 Creating a class for Calculator Part 1

--- classses/calculator.class.php
+++ classses/calculator.class.php
@@ -1,5 +1,14 @@
 <?php
   class Calculator {
-    
+    private $firstNumber;
+    private $secondNumber;
+    private $operation;
+
+    public function __construct($first, $second, $operation){
+      $this->firstNumber = $first;
+      $this->secondNumber = $second;
+      $this->operation = $operation;
+
+    }
   }
 ?>
